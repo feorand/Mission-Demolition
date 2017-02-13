af95095e38944b57fc4e09c5a50ebbce968ccda2
Pavel Prokofyev <feorand@gmail.com>
2017-02-13 14:17:24 +0500

    stuff

---
+++ Assets/MouseInteractor.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseInteractor : MonoBehaviour {
+	void OnMouseEnter() {
+		print("Mouse entered");
+	}
+
+	void OnMouseExit() {
+		print("Mouse exited");
+	}
+}
