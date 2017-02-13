b4cf02c9adec65cf90da32888adf48cfab5388ae
Pavel Prokofyev <feorand@gmail.com>
2017-02-13 14:50:41 +0500

    added mousedown event to slignshot

--- Assets/LaunchPointBehaviour.cs
+++ Assets/LaunchPointBehaviour.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class LaunchPointBehaviour : MonoBehaviour {
-
 	public void OnMouseEntered() {
 		gameObject.SetActive(true);
 	}
--- Assets/MouseInteractor.cs
+++ Assets/MouseInteractor.cs
@@ -2,7 +2,7 @@
 using UnityEngine.Events;
 
 public class MouseInteractor : MonoBehaviour {
-
+	public GameObject projectilePrefab;
 	public UnityEvent MouseEntered;
 	public UnityEvent MouseExited;
 
@@ -12,5 +12,13 @@ public class MouseInteractor : MonoBehaviour {
 
 	void OnMouseExit() {
 		MouseExited.Invoke();
+	}
+
+	public void OnMouseDown()
+	{
+		var projectile = Instantiate(projectilePrefab) as GameObject;
+		var launchPointPosition = GameObject.Find("Launch Point").transform.position;
+		projectile.transform.position = launchPointPosition;
+		projectile.GetComponent<Rigidbody>().isKinematic = true;
 	}
 }
