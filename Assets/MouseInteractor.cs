989c3236aafe3ac705b66ed7ac1561ed8248acaa
Pavel Prokofyev <feorand@gmail.com>
2017-02-13 15:54:10 +0500

    going home

--- Assets/MouseInteractor.cs
+++ Assets/MouseInteractor.cs
@@ -5,6 +5,7 @@ public class MouseInteractor : MonoBehaviour {
 	public GameObject projectilePrefab;
 	public UnityEvent MouseEntered;
 	public UnityEvent MouseExited;
+	public UnityEvent MouseDown;
 
 	void OnMouseEnter() {
 		MouseEntered.Invoke();
@@ -20,5 +21,10 @@ public class MouseInteractor : MonoBehaviour {
 		var launchPointPosition = GameObject.Find("Launch Point").transform.position;
 		projectile.transform.position = launchPointPosition;
 		projectile.GetComponent<Rigidbody>().isKinematic = true;
+		MouseDown.Invoke();
+	}
+
+	public void OnMouseUp() {
+		Destroy(GameObject.Find("Projectile(Clone)"));
 	}
 }
---
+++ Assets/ProjectileFiringBehaviour.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+public class ProjectileFiringBehaviour : MonoBehaviour {
+
+	Vector3 launchPosition;
+
+	public void Awake() { 
+		launchPosition = GameObject.Find("Launch Point").transform.position;
+	}
+
+	public void Update() {
+		var mouseCoords2D = Input.mousePosition;
+		mouseCoords2D.z = -Camera.main.transform.position.z;
+		var mouseCoords3D = Camera.main.ScreenToWorldPoint(mouseCoords2D);
+
+		var mouseDelta = mouseCoords3D - launchPosition;
+		var maxDistance = GameObject.Find("Slingshot").GetComponent<SphereCollider>().radius;
+
+		if (mouseDelta.magnitude > maxDistance) {
+			mouseDelta.Normalize();
+			mouseDelta *= maxDistance;
+		}
+
+		transform.Translate(launchPosition + mouseDelta);
+	}
+}
