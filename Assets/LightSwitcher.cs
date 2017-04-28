7312ce42cf3d9d0fdedc3014f09de246c222bd8c
Pavel Prokofyev <feorand@gmail.com>
2017-04-28 15:22:18 +0500

    Refactored

---
+++ Assets/LightSwitcher.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public class LightSwitcher : MonoBehaviour {
+	public GameObject launchPoint;
+
+	public void OnMouseEnter() {
+		launchPoint.SetActive(true);
+	}
+
+	public void OnMouseExit() {
+		launchPoint.SetActive(false);
+	}
+}
---
+++ Assets/ProjectileFiring.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+public class ProjectileFiring : MonoBehaviour {
+
+	Vector3 launchPosition;
+
+	public void Awake() { 
+		launchPosition = GameObject.Find("Launch Point").transform.position;
+	}
+
+	public void Update() {
+
+
+		//var mouseDelta = mouseCoords3D - launchPosition;
+		//var maxDistance = GameObject.Find("Slingshot").GetComponent<SphereCollider>().radius;
+
+		//if (mouseDelta.magnitude > maxDistance) {
+		//	mouseDelta.Normalize();
+		//	mouseDelta *= maxDistance;
+		//}
+
+		//transform.Translate(launchPosition + mouseDelta);
+	}
+}
